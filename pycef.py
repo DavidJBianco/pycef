b349d537eeccfae811f99c9b8faa663cacb08506
David J. Bianco <davidjbianco@gmail.com>
2016-07-11 19:21:20 -0400

    Added comments!  Also remove extraneous print statements.

--- pycef.py
+++ pycef.py
@@ -4,10 +4,19 @@ import sys
 import re
 import json
 
-def cef_parse(str):
 
+def cef_parse(str):
+    """
+    Parse a string in CEF format and return a dict with the header values
+    and the extension data.
+    """
+
+    # Create the empty dict we'll return later
     values = dict()
 
+    # This regex separates the string into the CEF header and the extension
+    # data.  Once we do this, it's easier to use other regexes to parse each
+    # part.
     header_re = r'(.*(?<!\\)\|){,7}(.*)'
     
     res = re.search(header_re, str)
@@ -15,8 +24,13 @@ def cef_parse(str):
         header = res.group(1)
         extension = res.group(2)
 
+        # Split the header on the "|" char.  Uses a negative lookbehind
+        # assertion to ensure we don't accidentally split on escaped chars,
+        # though.
         spl = re.split(r'(?<!\\)\|', header)
 
+        # Since these values are set by their position in the header, it's
+        # easy to know which is which.
         values["DeviceVendor"] = spl[1]
         values["DeviceProduct"] = spl[2]
         values["DeviceVersion"] = spl[3]
@@ -24,15 +38,22 @@ def cef_parse(str):
         values["DeviceName"] = spl[5]
         values["DeviceSeverity"] = spl[6]
         
-
+        # The first value is actually the CEF version, formatted like
+        # "CEF:#".  We split on the colon and use the second value as the
+        # version number.
         (cef, version) = spl[0].split(':')
         values["CEFVersion"] = version
-        
+
+        # The ugly, gnarly regex here finds a single key=value pair,
+        # taking into account multiple whitespaces, escaped '=' and '|'
+        # chars.  It returns an iterator of tuples.
         spl = re.findall(r'([^=\s]+)=((?:[\\]=|[^=])+)(?:\s|$)', extension)
         for i in spl:
+            # Split the tuples and put them into the dictionary
             values[i[0]] = i[1]
 
-    return     values
+    # Now we're done!
+    return values
 
 ###### Main ######
 if len(sys.argv) != 2:
@@ -43,8 +64,9 @@ file = sys.argv[1]
 
 for line in open(file, "r").readlines():
     line = line.rstrip('\n')
-    print line
 
+    # Read the file, and parse each line of CEF into a separate JSON document
+    # to stdout
     values = cef_parse(line)
     print json.dumps(values)
 
