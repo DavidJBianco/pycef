35c17474d87fcb76902fd53893393be5164f9816
David J. Bianco <davidjbianco@gmail.com>
2018-09-24 12:20:47 -0400

    Now compatible with both Python 2 and 3!

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ setup(name='pycef',
       author='David J. Bianco',
       author_email='davidjbianco@gmail.com',
       packages=['pycef'],
-      tests_require=['pytest'],
+      tests_require=['future', 'pytest'],
       setup_requires=['pytest-runner'],
-      install_requires=[],
-      zip_safe=True
+      install_requires=['future'],
+      zip_safe=True,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 2.7"
+      ]
 )
